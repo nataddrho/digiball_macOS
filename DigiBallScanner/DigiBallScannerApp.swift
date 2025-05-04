2ba0d5add12a69934255bbf80593d04a1945fa4e
Nathan Rhoades <nataddrho@gmail.com>
2025-05-04 09:14:55 -0400

    Initial commit

--- DigiBallScanner/DigiBallScannerApp.swift
+++ DigiBallScanner/DigiBallScannerApp.swift
@@ -7,11 +7,20 @@
 
 import SwiftUI
 
+class GlobalSettings: ObservableObject {
+    @Published var lastShotNumber: UInt8 = 255
+    func updateShotNumber(value: UInt8) {
+        lastShotNumber = value
+    }
+}
+
 @main
 struct DigiBallScannerApp: App {
+    var settings = GlobalSettings()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(settings)
         }
     }
 }
